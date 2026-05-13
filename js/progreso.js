515d52825f7599b333cea1adc14fcc1cfce4ac36
Luis Francisco Castrillo Navarro <luisks3yo@gmail.com>
2026-05-13 10:14:09 -0500

    feat: progreso conectado al backend

--- js/progreso.js
+++ js/progreso.js
@@ -2,41 +2,46 @@ function obtenerUsuario() {
   return JSON.parse(localStorage.getItem("usuarioActivo"));
 }
 
-function obtenerClaveProgreso() {
+async function guardarProgreso(valor) {
   const usuario = obtenerUsuario();
-  return "progresoMatematicas_" + usuario.correo;
-}
 
-function obtenerProgreso() {
-  return JSON.parse(localStorage.getItem(obtenerClaveProgreso())) || 0;
-}
-
-function guardarProgreso(valor) {
-  localStorage.setItem(obtenerClaveProgreso(), JSON.stringify(valor));
-}
-
-function actualizarBarra() {
-  const barra = document.querySelector(".progress");
-  if (barra) {
-    const progreso = obtenerProgreso();
-    barra.style.width = progreso + "%";
-    barra.textContent = progreso + "%";
+  try {
+    await fetch("http://localhost:3000/api/progreso", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correo: usuario.correo,
+        progreso: valor,
+      }),
+    });
+  } catch (error) {
+    console.log(error);
   }
 }
 
-function marcarTema(boton, valor = 30) {
-  let progreso = obtenerProgreso();
+async function marcarTema(boton, valor = 30) {
+  let progreso = Number(document.querySelector(".progress").dataset.valor || 0);
 
   if (!boton.classList.contains("completado")) {
     progreso += valor;
-    if (progreso > 100) progreso = 100;
+
+    if (progreso > 100) {
+      progreso = 100;
+    }
 
     boton.textContent = "Completado";
     boton.style.background = "green";
     boton.classList.add("completado");
 
-    guardarProgreso(progreso);
-    actualizarBarra();
+    const barra = document.querySelector(".progress");
+
+    barra.style.width = progreso + "%";
+    barra.textContent = progreso + "%";
+    barra.dataset.valor = progreso;
+
+    await guardarProgreso(progreso);
   }
 }
 
@@ -44,12 +49,10 @@ function responderReto(correcta) {
   const mensaje = document.getElementById("mensajeReto");
 
   if (correcta) {
-    mensaje.textContent = "Correcto ✅";
+    mensaje.textContent = "Correcto ";
     mensaje.style.color = "green";
   } else {
-    mensaje.textContent = "Intenta de nuevo ❌";
+    mensaje.textContent = "Intenta de nuevo";
     mensaje.style.color = "red";
   }
 }
-
-window.onload = actualizarBarra;
